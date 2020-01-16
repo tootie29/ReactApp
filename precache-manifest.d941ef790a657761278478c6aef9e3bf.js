435ae1b98024130df24928dd8a7b3e2e1527288c
tootie29 <chardmedina@gmail.com>
2020-01-17 02:37:35 +0800

    Updates

---
+++ precache-manifest.d941ef790a657761278478c6aef9e3bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bc373845afa5e464027770f44428da",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "701800cb5055f3130be5",
+    "url": "/ReactApp/static/css/main.d0e49d5a.chunk.css"
+  },
+  {
+    "revision": "ec1946e5e6bd6b93e9ba",
+    "url": "/ReactApp/static/js/2.36f7e9f1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ReactApp/static/js/2.36f7e9f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "701800cb5055f3130be5",
+    "url": "/ReactApp/static/js/main.b13a934e.chunk.js"
+  },
+  {
+    "revision": "3a3d376853f8cd6e22bd",
+    "url": "/ReactApp/static/js/runtime-main.dfb62845.js"
+  }
+]);
